129c8d2646cb5a7be1e58b7c4d396ebb88e644eb
My Name <me@example.com>
2024-05-08 20:10:43 -0500

    changed \'recommendations\' to \'recommendation_desc\'

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def main():
     r = requests.get('https://store.steampowered.com/curator/42948398-cultcraft-%2528on-dingus%2529/#browse')
     soup = BeautifulSoup(r.content, 'html.parser')
     s = soup.find('div', class_='browse_content') 
-    content = s.find_all('div', class_='recommendation') 
+    content = s.find_all('div', class_='recommendation_desc') 
 
     print(content)
 
